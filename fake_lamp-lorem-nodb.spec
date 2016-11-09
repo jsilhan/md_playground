c28977874201d4b0079184a5ade472ead294c796
Jan Silhan <jsilhan@redhat.com>
2016-11-09 21:26:31 +0100

    better support specification

--- fake_lamp-lorem-nodb.spec
+++ fake_lamp-lorem-nodb.spec
@@ -10,16 +10,27 @@ BuildArch: noarch
 
 %global module_tag %{name}_%{stream}_%{release}
 
-# tags from Modulemd.yaml
+# tags from module_md.yaml
 Provides: md.stream = %{stream}
 Provides: md.profile = %{profile}
 Provides: md.api = fake-httpd
 Provides: md.api = fake-mariadb
 Provides: md.api = fake-php
 
-# from Modulemd.yaml, data/profiles/<profile>/rpms
+# from module_md.yaml, data/profiles/<profile>/rpms
 Requires: fake-httpd = 2.2-1.%{module_tag}
 Requires: fake-php = 6.0-1.%{module_tag}
+
+
+# TAGS not yet in module_md.yaml
+# better API support specification / md.api replacement?
+Provides: md.support_cli(/usr/bin/php) = 2
+Provides: md.support_api(/usr/lib64/php-zts/) = 2
+Provides: md.support_api(/usr/lib64/php/) = 1
+# information about release cycle and supportability?
+Provides: md.support_till = 2018.07.01
+Provides: md.release_cycle_months = 48
+
 
 %description
 %{summary}
--- genrpms.sh
+++ genrpms.sh
@@ -1,4 +1,6 @@
-#!/bin/bash 
+#!/bin/bash
+
+set -e
 
 target=/home/jsilhan/Projects/rmps/repo
 rpmbuild -ba fake_lamp-lorem-nodb.spec
@@ -7,4 +9,19 @@ mv /home/jsilhan/rpmbuild/SRPMS/fake_lamp-lorem_default-1.src.rpm $target
 mv /home/jsilhan/rpmbuild/RPMS/noarch/fake_lamp-lorem_default-1.noarch.rpm $target
 mv /home/jsilhan/rpmbuild/SRPMS/fake_lamp-lorem_nodb-1.src.rpm $target
 mv /home/jsilhan/rpmbuild/RPMS/noarch/fake_lamp-lorem_nodb-1.noarch.rpm $target
+
+rpmbuild -ba fake_lamp-ipsum-nodb.spec
+rpmbuild -ba fake_lamp-ipsum-default.spec
+mv /home/jsilhan/rpmbuild/SRPMS/fake_lamp-ipsum_default-1.src.rpm $target
+mv /home/jsilhan/rpmbuild/RPMS/noarch/fake_lamp-ipsum_default-1.noarch.rpm $target
+mv /home/jsilhan/rpmbuild/SRPMS/fake_lamp-ipsum_nodb-1.src.rpm $target
+mv /home/jsilhan/rpmbuild/RPMS/noarch/fake_lamp-ipsum_nodb-1.noarch.rpm $target
+
+rpmbuild -ba fake_lamp-dolor-nodb.spec
+rpmbuild -ba fake_lamp-dolor-default.spec
+mv /home/jsilhan/rpmbuild/SRPMS/fake_lamp-dolor_default-1.src.rpm $target
+mv /home/jsilhan/rpmbuild/RPMS/noarch/fake_lamp-dolor_default-1.noarch.rpm $target
+mv /home/jsilhan/rpmbuild/SRPMS/fake_lamp-dolor_nodb-1.src.rpm $target
+mv /home/jsilhan/rpmbuild/RPMS/noarch/fake_lamp-dolor_nodb-1.noarch.rpm $target
+
 createrepo_c $target
